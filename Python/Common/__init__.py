d8af5e45c10479ed7a30aad30a36c9da055c84c7
maciekniewielki <kadlubken5@gmail.com>
2022-12-13 07:09:27 +0100

    Added flatten to Commons

--- Python/Common/__init__.py
+++ Python/Common/__init__.py
@@ -5,9 +5,11 @@ def input_as_string(filename):
     with open(filename) as f:
         return f.read().rstrip("\n")
 
+
 def input_as_lines(filename):
     """Return a list where each line in the input file is an element of the list"""
     return input_as_string(filename).split("\n")
+
 
 def input_as_ints(filename):
     """Return a list where each line in the input file is an element of the list, converted into an integer"""
@@ -15,10 +17,12 @@ def input_as_ints(filename):
     line_as_int = lambda l: int(l.rstrip('\n'))
     return list(map(line_as_int, lines))
 
+
 def input_as_one_line_ints(filename):
     """Return a list where it splits the file by comma and converts each entry into an integer"""
     entries = input_as_string(filename).split(",")
     return list(map(int, entries))
+
 
 def input_as_chunks(filename):
     """Return a list where each chunk of lines in the input file is an element of the list"""
@@ -36,7 +40,13 @@ def input_as_chunks(filename):
     chunks.append(curr_chunk)
     return chunks
 
+
 def input_as_chunked_ints(filename):
     """Return a list where each chunk of lines in the input file is a separate list of integers"""
     chunks = input_as_chunks(filename)
     return [list(map(int, c)) for c in chunks]
+
+
+def flatten(l):
+    """Return a flattened list"""
+    return [item for sublist in l for item in sublist]
